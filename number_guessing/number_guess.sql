fd5581b26d415ad80cb7a0a737632760fd8a7010
Robby <55962538+robby-o@users.noreply.github.com>
2024-01-16 14:30:30 -0800

    Create number_guess.sql

---
+++ number_guessing/number_guess.sql
@@ -0,0 +1,87 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.users (
+    username character varying(30) NOT NULL,
+    games_played integer DEFAULT 0,
+    best_game integer
+);
+
+
+ALTER TABLE public.users OWNER TO freecodecamp;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.users VALUES ('rob', 1, 10);
+INSERT INTO public.users VALUES ('user_1705443263920', 2, 153);
+INSERT INTO public.users VALUES ('user_1705443263921', 5, 108);
+INSERT INTO public.users VALUES ('user_1705443293089', 2, 599);
+INSERT INTO public.users VALUES ('user_1705443293090', 5, 333);
+INSERT INTO public.users VALUES ('user_1705443392366', 2, 422);
+INSERT INTO public.users VALUES ('user_1705443392367', 5, 42);
+INSERT INTO public.users VALUES ('user_1705443442253', 2, 157);
+INSERT INTO public.users VALUES ('user_1705443442254', 5, 2);
+INSERT INTO public.users VALUES ('user_1705443456145', 2, 618);
+INSERT INTO public.users VALUES ('user_1705443456146', 5, 626);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (username);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
