b9432b66bd9b5c9f2da10ac106cee0360da9f6bb
Robby <55962538+robby-o@users.noreply.github.com>
2024-01-16 15:25:05 -0800

    Update number_guess.sh

--- number_guessing/number_guess.sh
+++ number_guessing/number_guess.sh
@@ -2,7 +2,7 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-function GUESSING {
+function GUESSING(){
     local username=$1
     local secret_number=$((RANDOM % 1000 + 1))
     local attempts=0
@@ -22,17 +22,18 @@ function GUESSING {
 
         if [ "$guess" -eq "$secret_number" ]; then
             # Check if it's a new best game
-            local user_info=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$username'")
-            IFS="|" read games_played best_game <<< $user_info
+            # local user_info=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$username'")
+            local user_id=$($PSQL "SELECT user_id FROM users WHERE username = '$username'")
+            local best_game=$($PSQL "SELECT best_game FROM games WHERE user_id = $user_id")
+            # IFS="|" read games_played best_game <<< $user_info
 
             if [ -z "$best_game" ] || [ "$attempts" -lt "$best_game" ]; then
                 # Update database with the new best game
-                echo "$attempts number of attempts"
-                local insert_best_game=$($PSQL "UPDATE users SET best_game = $attempts WHERE username = '$username'")
+                local insert_best_game=$($PSQL "UPDATE games SET best_game = $attempts WHERE user_id = '$user_id'")
             fi
 
             # Update database with the number of games played
-            local update_user=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE username = '$username'")
+            local update_user=$($PSQL "UPDATE games SET games_played = games_played + 1 WHERE user_id = '$user_id'")
             echo "You guessed it in $attempts tries. The secret number was $secret_number. Nice job!"
             break
         elif [ "$guess" -lt "$secret_number" ]; then
@@ -44,27 +45,28 @@ function GUESSING {
 }
 
 DISPLAY() {
+    echo "Enter your username:"
+    read username
 
-echo "Enter your username:"
-read username
+    # Check if the username exists in the database
+    # user_info=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username = '$username'")
+    user_id=$($PSQL "SELECT user_id FROM users WHERE username='$username'")
+    if [ -n "$user_id" ]; then
+        # User exists
+        user_info=$($PSQL "SELECT games_played, best_game FROM games WHERE user_id = '$user_id'")
+        IFS="|" read games_played best_game <<< $user_info
+        echo "gp $games_played, bg $best_game"
+        echo -e "\nWelcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
+    else
+        # New user
+        insert_user=$($PSQL "INSERT INTO users (username) VALUES ('$username')")
+        user_id=$($PSQL "SELECT user_id FROM users WHERE username='$username'")
+        insert_game=$($PSQL "INSERT INTO games (user_id) VALUES('$user_id')")
+        echo "Welcome, $username! It looks like this is your first time here."
+    fi
 
-# Check if the username exists in the database
-user_info=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username = '$username'")
-
-if [ -n "$user_info" ]; then
-    # User exists
-    IFS="|" read user games_played best_game <<< $user_info
-    echo "Welcome back, $user! You have played $games_played games, and your best game took $best_game guesses."
-
-else
-    # New user
-    insert_user=$($PSQL "INSERT INTO users (username) VALUES ('$username')")
-    echo "Welcome, $username! It looks like this is your first time here."
-fi
-
-# Call the GUESSING function
-GUESSING $username
-
+    # Call the GUESSING function
+    GUESSING $username
 }
 
 DISPLAY
